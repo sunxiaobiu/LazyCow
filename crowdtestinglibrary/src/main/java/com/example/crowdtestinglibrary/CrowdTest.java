51c9b38533746e3885b574348dc40340616b3fb9
Jimmy Low <lemoning.low@gmail.com>
2021-08-15 09:05:31 +0800

    fixed compilation errors and syntax errors due to missing imports

--- crowdtestinglibrary/src/main/java/com/example/crowdtestinglibrary/CrowdTest.java
+++ crowdtestinglibrary/src/main/java/com/example/crowdtestinglibrary/CrowdTest.java
@@ -1,31 +1,40 @@
 package com.example.crowdtestinglibrary;
 
+import android.content.Intent;
 import java.util.concurrent.TimeUnit;
 import android.util.Log;
+import android.content.Context;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
 import okhttp3.Request;
 import okhttp3.Callback;
-import com.example.crowdtestlibrary.model.DeviceInfo;
+import okhttp3.FormBody;
+import okhttp3.Response;
+import com.example.crowdtestinglibrary.model.DeviceInfo;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import android.provider.Settings;
-
-
-
+import android.os.Build;
 
 public class CrowdTest {
     private static String deviceId = "";
+    private static Context context;
+    private static String TAG;
+    private static String patchAPKName = "app-debug-patch_signed_7zip.apk";
 
-    public CrowdTest() {
-        deviceId = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID) + "_" + Build.SERIAL;
+
+    public CrowdTest(Context context, String TAG) {
+        this.context = context;
+        this.TAG = TAG;
+        deviceId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID) + "_" + Build.SERIAL;
     }
 
     public void generatePatchAPK() {
-        DeviceInfo deviceInfo = new DeviceInfo(getApplicationContext());
+        DeviceInfo deviceInfo = new DeviceInfo(context);
         deviceInfo.setDeviceId(deviceId);
 
         OkHttpClient.Builder builder = new OkHttpClient.Builder().connectTimeout(20, TimeUnit.SECONDS)
