f7caeb157c13626bd2e72d4de3d328221165be3d
Shu Hui Anna Zhang <szha0082@student.monash.edu>
2021-10-05 14:22:38 +1100

    Reverted changes

--- app/src/main/java/tinker/sample/android/service/AlarmReceiver.java
+++ app/src/main/java/tinker/sample/android/service/AlarmReceiver.java
@@ -3,16 +3,11 @@ package tinker.sample.android.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent i = new Intent(context, LongRunningService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(i);
-        } else {
-            context.startService(i);
-        }
+        context.startService(i);
     }
 }
