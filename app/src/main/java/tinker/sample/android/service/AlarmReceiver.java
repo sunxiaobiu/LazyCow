6449088482b43dba91829cf56ad2a4fd2fc72940
Shu Hui Anna Zhang <szha0082@student.monash.edu>
2021-10-05 12:59:30 +1100

    Updated alarm receiver for API 26+

--- app/src/main/java/tinker/sample/android/service/AlarmReceiver.java
+++ app/src/main/java/tinker/sample/android/service/AlarmReceiver.java
@@ -3,11 +3,16 @@ package tinker.sample.android.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent i = new Intent(context, LongRunningService.class);
-        context.startService(i);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(i);
+        } else {
+            context.startService(i);
+        }
     }
 }
